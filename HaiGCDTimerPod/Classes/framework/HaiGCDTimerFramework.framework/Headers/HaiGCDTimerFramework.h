dd7d1a559b4de8ba1c4feb62d04acecb3f20bdbf
Ios Developer <ios_developer@ios-developerdemacbook-pro.local>
2018-01-04 09:50:01 +0800

    1.0

---
+++ HaiGCDTimerPod/Classes/framework/HaiGCDTimerFramework.framework/Headers/HaiGCDTimerFramework.h
@@ -0,0 +1,20 @@
+//
+//  HaiGCDTimerFramework.h
+//  HaiGCDTimerFramework
+//
+//  Created by Ios_Developer on 2018/1/4.
+//  Copyright © 2018年 hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HaiTimer.h"
+
+//! Project version number for HaiGCDTimerFramework.
+FOUNDATION_EXPORT double HaiGCDTimerFrameworkVersionNumber;
+
+//! Project version string for HaiGCDTimerFramework.
+FOUNDATION_EXPORT const unsigned char HaiGCDTimerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HaiGCDTimerFramework/PublicHeader.h>
+
+
